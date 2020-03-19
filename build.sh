06991ef6d80c098164e27927c10d5fa1abef7c3e
Tim Myers <tdmsportplayer@yahoo.com>
2020-03-18 17:16:44 -0700

    adding build.py to homework

---
+++ build.sh
@@ -0,0 +1,11 @@
+cat templates/top.html content/index.html templates/bottom.html
+> docs/index.html
+
+cat templates/top.html content/about-me.html templates/bottom.html
+> docs/about-me.html
+
+cat templates/top.html content/resume.html templates/bottom.html
+> docs/resume.html
+
+cat templates/top.html content/contact.html templates/bottom.html
+> docs/contact.html
